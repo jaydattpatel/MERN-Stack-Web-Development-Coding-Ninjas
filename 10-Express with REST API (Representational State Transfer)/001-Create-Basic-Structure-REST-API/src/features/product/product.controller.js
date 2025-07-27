d2e5429de754e83b1429d568e5aa70b67e22061b
Jaydatt <124486498+jaydattpatel@users.noreply.github.com>
2025-07-27 19:38:23 +0530

    updated

--- 10-Express with REST API (Representational State Transfer)/001-Create-Basic-Structure-REST-API/src/features/product/product.controller.js
+++ 10-Express with REST API (Representational State Transfer)/001-Create-Basic-Structure-REST-API/src/features/product/product.controller.js
@@ -1,22 +1,14 @@
 import ProductModel from "./productModel.js";
 
-export default class ProductController{
+export default class ProductController {
+  getAllProducts(req, res) {
+    const products = ProductModel.getAll();
+    res.status(200).send(products);
+  }
 
-    getAllProducts(req,res){
-        const products = ProductModel.getAll();
-        res.status(200).send(products);
-    }
+  addProduct(req, res) {}
 
-    addProduct(req, res){
+  rateProduct(req, res) {}
 
-    }
-
-    rateProduct(req,res){
-
-    }
-
-    getOneProduct(req,res){
-        
-    }
-
+  getOneProduct(req, res) {}
 }
--- 10-Express with REST API (Representational State Transfer)/003-Creates-SignUp-SignIn-API-for-users/src/features/user/user.controller.js
+++ 10-Express with REST API (Representational State Transfer)/003-Creates-SignUp-SignIn-API-for-users/src/features/user/user.controller.js
@@ -1,18 +1,18 @@
-import UserModel from './user.model.js';
+import UserModel from "./user.model.js";
 
 export default class UserController {
   signUp(req, res) {
-    const {name,email,password,type} = req.body;
-    const user = UserModel.SignUp(name,email,password,type);
+    const { name, email, password, type } = req.body;
+    const user = UserModel.SignUp(name, email, password, type);
     res.status(201).send(user);
   }
 
   signIn(req, res) {
-    const result = UserModel.SignIn(req.body.email,req.body.password);
+    const result = UserModel.SignIn(req.body.email, req.body.password);
     if (!result) {
-      return res.status(400).send('Incorrect Credentials');
+      return res.status(400).send("Incorrect Credentials");
     } else {
-      return res.send('Login Successful');
+      return res.send("Login Successful");
     }
   }
 }
